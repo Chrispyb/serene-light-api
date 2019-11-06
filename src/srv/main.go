43b9e36db753d9c2e284cdb13bedacff75ad17f2
Chris <cabrady92@gmail.com>
2019-11-05 22:40:43 -0500

    Added a common package

--- src/srv/main.go
+++ src/srv/main.go
@@ -4,10 +4,17 @@ import (
     "fmt"
     "log"
     "net/http"
+    "encoding/json"
+    "serene-light-api/src/common"
 )
 
+
 func getupdates(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+
+    m := message.Message{"Alice", "Hello", 1294706395881547000}
+    b, _ := json.Marshal(m)
+
+    fmt.Fprintf(w, string(b))
 }
 
 func postupdates(w http.ResponseWriter, r *http.Request) {
